5e7f537be7c18c6108360041eb8192f026bf587b
Pier9730 <96286206+Pier9730@users.noreply.github.com>
2022-03-20 22:29:17 +0100

    Create Esercizio 4.py

---
+++ Esercizio 4.py
@@ -0,0 +1,9 @@
+string= 'Puoi modificare la tua scelta, in qualsiasi momento, accedendo al pannello delle preferenze'
+
+y=string.count('c')
+x=string.upper()
+z=string.find('momento')
+
+print('Maiuscolo: ', x, '\nOccorenza c: ', y, '\nMomento: ', z)
+
+
